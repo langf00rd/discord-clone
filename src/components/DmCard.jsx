5454f3f14f6073df90d53f19116977ab3379a992
Langford Quarshie <langfordquarshie21@gmail.com>
2022-02-16 00:24:28 +0000

    routing works

--- src/components/DmCard.jsx
+++ src/components/DmCard.jsx
@@ -1,12 +1,20 @@
 const DmCard = (props) => {
+    let randomID = (Math.random() * 100000).toFixed(0)
+
+    const changeUrl = () => {
+        window.history.replaceState(null, props.name, `/channels/@me/${randomID}`)
+        window.location.hash = props.name
+    }
+
     return (
-        <div className="dm-card">
+        <div className={window.location.hash === `#${props.name}` ? 'active-room dm-card' : 'dm-card'
+        } onClick={changeUrl} >
             <div className="dm-avatar-container">
                 <img src={props.avatar} className='dm-avatar' alt="" />
                 <div className="status" id={props.status}></div>
             </div>
-            <p>{props.name}</p>
-        </div>
+            <p className="fade-text">{props.name}</p>
+        </div >
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,13 +19,38 @@ import sticker from '../assets/icons/sticker.svg'
 import smiley from '../assets/icons/smiley.svg'
 import friends from '../assets/icons/friends.svg'
 import nitro from '../assets/icons/nitro.svg'
+import discord from '../assets/icons/discord.svg'
+import React, { useState } from "react"
+import GUN from "https://cdn.skypack.dev/gun";
 
 const Home = () => {
-    const inputPlaceholder = 'Message @langford'
+    const [roomName, setroomName] = useState()
+    const [placeholder, setPlaceholder] = useState('Message...')
+    const [messageText, setMessageText] = useState('')
+
+    // const server = require('http').createServer().listen(8080);
+    // const gun = GUN({ web: server });
+
+    React.useEffect(() => {
+
+        console.log(GUN())
+
+
+        window.addEventListener('hashchange', function () {
+            let name = window.location.hash.replace('#', "")
+            // console.log(window.location.pathname.substring(6, 10))
+            setroomName(name)
+            setPlaceholder(`Message ${name}`)
+            setMessageText('')
+        })
+
+
+    }, [])
+
     return (
         <main>
             <div className="side-bar">
-                <RoomAvatar avatar={avatar1} />
+                <RoomAvatar avatar={discord} className='home-btn' />
                 <RoomAvatar avatar={avatar2} />
                 <RoomAvatar avatar={avatar3} />
                 <RoomAvatar avatar={avatar4} />
@@ -50,13 +75,13 @@ const Home = () => {
                     <div className="space-50"></div>
                     <div className="space-10"></div>
                     <div className="conversations-container">
-                        <div className="flex">
+                        <div className="flex hover-effect">
                             <img src={friends} className='svg' alt="" />
                             <div className="space-10"></div>
                             <p>Fiends</p>
                         </div>
                         <div className="space-20"></div>
-                        <div className="flex">
+                        <div className="flex hover-effect">
                             <img src={nitro} className='svg' alt="" />
                             <div className="space-10"></div>
                             <p>Nitro</p>
@@ -64,15 +89,10 @@ const Home = () => {
                         <div className="space-30"></div>
                         <b className="fade-text">DIRECT MESSAGES</b>
                         <div className="space-10"></div>
-                        <DmCard name='Lo-fi beats' avatar={avatar3} status='online' />
+                        <DmCard name='Lo-fi' avatar={avatar3} status='online' />
                         <DmCard name='Rafeh' avatar={avatar1} status='offline' />
-                        <DmCard name='Rafeh' avatar={avatar1} status='offline' />
-                        <DmCard name='Langford' avatar={avatar2} status='online' />
-                        <DmCard name='Lo-fi beats' avatar={avatar3} status='online' />
                         <DmCard name='Buildspace' avatar={avatar4} status='offline' />
-                        <DmCard name='Langford' avatar={avatar2} status='offline' />
-                        <DmCard name='Lo-fi beats' avatar={avatar3} status='online' />
-                        <DmCard name='Rafeh' avatar={avatar1} status='offline' />
+                        <DmCard name='Langford' avatar={avatar2} status='online' />
                     </div>
                 </div>
 
@@ -81,7 +101,7 @@ const Home = () => {
                         <div className="flex room-name">
                             <img src={at} className='svg' alt="" />
                             <div className="space-10"></div>
-                            <h3>Langford</h3>
+                            <h3>{roomName}</h3>
                             <div className="space-10"></div>
                             <div className="status" id="online"></div>
                         </div>
@@ -107,19 +127,14 @@ const Home = () => {
 
                     <div className="messages-main-container">
                         <MessageCard avatar={avatar1} sender='Langford' timestamp='02/02/2022' content='Hello world' />
-                        <MessageCard avatar={avatar2} sender='Rafeh' timestamp='06/02/2022' content='Venus is on fire' />
-                        <MessageCard avatar={avatar1} sender='Langford' timestamp='02/02/2022' content='Hello world' />
-                        <MessageCard avatar={avatar2} sender='Rafeh' timestamp='06/02/2022' content='Venus is on fire' />
-                        <MessageCard avatar={avatar1} sender='Langford' timestamp='02/02/2022' content='Hello world' />
-                        <MessageCard avatar={avatar2} sender='Rafeh' timestamp='06/02/2022' content='Venus is on fire' />
-                        <MessageCard avatar={avatar1} sender='Langford' timestamp='02/02/2022' content='Hello world' />
-                        <MessageCard avatar={avatar2} sender='Rafeh' timestamp='06/02/2022' content='Venus is on fire' />
                     </div>
 
                     <div className="chat-input-container">
                         <div className="chat-input-wrapper">
                             <img src={plusFilled} className='svg' />
-                            <textarea type="text" className="input-box" placeholder={inputPlaceholder} ></textarea>
+
+                            <textarea value={messageText} onChange={(e) => setMessageText(e.target.value)} type="text" className="input-box" placeholder={placeholder} ></textarea>
+
                             <div className="space-20"></div>
                             <img src={gift} className='svg' />
                             <div className="space-20"></div>
