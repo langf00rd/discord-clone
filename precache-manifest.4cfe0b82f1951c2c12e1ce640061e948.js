85b785ba2f9d5cf2d9a8189c8cc834ab53f5526a
Langford Kwabena <55645613+langfordquarshie21@users.noreply.github.com>
2022-02-19 22:53:47 +0000

    Add files via upload

---
+++ precache-manifest.4cfe0b82f1951c2c12e1ce640061e948.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ee5e6162289dc48053f",
+    "url": "/static/css/main.9e36f9cf.chunk.css"
+  },
+  {
+    "revision": "8ee5e6162289dc48053f",
+    "url": "/static/js/main.8ee5e616.chunk.js"
+  },
+  {
+    "revision": "a974d6237fe194dcd366",
+    "url": "/static/js/runtime~main.a974d623.js"
+  },
+  {
+    "revision": "3011338b5a845b780c60",
+    "url": "/static/js/2.3011338b.chunk.js"
+  },
+  {
+    "revision": "965e412a279ae96a1833",
+    "url": "/static/js/3.965e412a.chunk.js"
+  },
+  {
+    "revision": "1bed957cfe66687531da7873dbade434",
+    "url": "/static/media/avatar-1.1bed957c.webp"
+  },
+  {
+    "revision": "97c0ececa578975926776fc1efd2bc3e",
+    "url": "/static/media/avatar-3.97c0ecec.webp"
+  },
+  {
+    "revision": "5b283ecfa042832e1090ac2a96f80829",
+    "url": "/static/media/avatar-4.5b283ecf.webp"
+  },
+  {
+    "revision": "b6c4c6c18de7f3dc3c824df0c3734763",
+    "url": "/static/media/plus-filled.b6c4c6c1.svg"
+  },
+  {
+    "revision": "89c5be8f029739298d20c80f43c67146",
+    "url": "/static/media/person-plus.89c5be8f.svg"
+  },
+  {
+    "revision": "c166b772615fb6b6471dd1b46d592ed5",
+    "url": "/static/media/sticker.c166b772.svg"
+  },
+  {
+    "revision": "145f291b3ef8f00c974873716ac6c654",
+    "url": "/static/media/friends.145f291b.svg"
+  },
+  {
+    "revision": "20d82723738b77171579c16e98b39d77",
+    "url": "/static/media/smiley.20d82723.svg"
+  },
+  {
+    "revision": "b5296632d0d3edc2bf3641a05fe14deb",
+    "url": "/static/media/video.b5296632.svg"
+  },
+  {
+    "revision": "938c75e6288733aed8a8c1698945e0b1",
+    "url": "/static/media/inbox.938c75e6.svg"
+  },
+  {
+    "revision": "a33a5b05f20bb9149c23713165eb48b2",
+    "url": "/static/media/phone.a33a5b05.svg"
+  },
+  {
+    "revision": "25346e8da6caeec56335523a343e6af5",
+    "url": "/static/media/nitro.25346e8d.svg"
+  },
+  {
+    "revision": "7b5ce1182504209e7531c7783a3b821c",
+    "url": "/static/media/gift.7b5ce118.svg"
+  },
+  {
+    "revision": "47dc5cfa3a1e169745cbaa972947ee3c",
+    "url": "/static/media/help.47dc5cfa.svg"
+  },
+  {
+    "revision": "997cd7fe93e01bda86c7cc75734df871",
+    "url": "/static/media/gif.997cd7fe.svg"
+  },
+  {
+    "revision": "dc14404a080484fd957f7ad0cf2eff62",
+    "url": "/static/media/pin.dc14404a.svg"
+  },
+  {
+    "revision": "68d22423713906d7c7b17cba42836ba7",
+    "url": "/static/media/at.68d22423.svg"
+  },
+  {
+    "revision": "e1451f54055b6aab4711c8f9a87ad0b6",
+    "url": "/index.html"
+  }
+];
